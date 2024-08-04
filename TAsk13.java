e137ab9abc5f6f177ac91a1a40ed0b7dd49a3ff6
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-08-04 17:31:26 +0500

    TAsk13

---
+++ TAsk13.java
@@ -0,0 +1,20 @@
+package LinkedListLesson;
+
+import java.util.LinkedList;
+
+public class TAsk13 {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("White");
+        list.add("Brown");
+        list.add("Black");
+        list.add("Green");
+        list.add("Orange");
+
+
+
+    }
+}

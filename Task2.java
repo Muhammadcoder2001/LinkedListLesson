3b4693f75f399b09b99ed9a60b4eef0afdebf8e6
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-08-04 16:53:10 +0500

    Task2

---
+++ Task2.java
@@ -0,0 +1,23 @@
+package LinkedListLesson;
+
+import java.util.LinkedList;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        LinkedList <String> list = new LinkedList<>();
+
+        list.add("Abdulloh");
+        list.add("Hafizullo");
+        list.add("Sardor");
+        list.add("Abdulbosit");
+        list.add("Habibullo");
+        list.add("Samandar");
+
+        for(String element : list){
+            System.out.println(element);
+
+        }
+    }
+}

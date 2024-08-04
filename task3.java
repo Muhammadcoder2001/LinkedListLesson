ea9c212898ba6b515b54696e7b19edd343944d83
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-08-04 16:56:16 +0500

    Task

---
+++ task3.java
@@ -0,0 +1,23 @@
+package LinkedListLesson;
+
+import java.util.LinkedList;
+
+public class task3 {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+
+        list.add("Abdulloh");
+        list.add("Hafizullo");
+        list.add("Sardor");
+        list.add("Abdulbosit");
+        list.add("Habibullo");
+        list.add("Samandar");
+
+        list.set(0, "Pink");
+        System.out.println(list);
+
+
+    }
+}
